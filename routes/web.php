51e7c57d7a699f1856cdf7c232b6da4c1edc074b
soru <ityouna007@gmail.com>
2025-01-18 21:50:55 +0900

    リソースルートを一行にまとめると

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,3 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/create',[PostController::class, 'create']);
-Route::post('/posts', [PostController::class, 'store']);
-Route::get('/posts/{id}', [PostController::class, 'show']);
-Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
-Route::put('/posts/{id}', [PostController::class, 'update']);
-Route::delete('/posts/{id}', [PostController::class, 'destroy']);
