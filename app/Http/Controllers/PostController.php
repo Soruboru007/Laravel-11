8649b8efce22d028794a404577f846edc71ab300
soru <ityouna007@gmail.com>
2025-01-18 21:26:48 +0900

    Controllerの練習 PostControllerの作成

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * リソースの一覧を表示する。
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * 新しいリソースを作成するフォームを表示する。
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * 新しく作成されたリソースを保存する。
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * 指定されたリソースを表示する。
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * 指定されたリソースを編集するフォームを表示する。
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * 指定されたリソースを更新する。
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * 指定されたリソースを削除する。
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Route::post('/', function (Request $request) {
-    dd($request->all());
+	//新しいポストを作成
+	//入ってくるリクエストを検証する
+	dd($request->all());
 });
 
 Route::put('/{id}', function (Request $request, $id) {
-    return $id;
+	return $id;
 });
 
 Route::delete('/{id}', function (Request $request, $id) {
-    return $id;
+	return $id;
 });
 
 Route::get('/test', function () {
 
-    $test = "Test again";
-    return "<h1>$test</h1>";
+	$test = "Test again";
+	return "<h1>$test</h1>";
 
 });
 
