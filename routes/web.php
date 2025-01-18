7d599130ecddfa219bb15f546fafb856b045eeac
soru <ityouna007@gmail.com>
2025-01-18 21:58:57 +0900

    リソースルートを一行にした

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::resource('posts', PostController::class);
