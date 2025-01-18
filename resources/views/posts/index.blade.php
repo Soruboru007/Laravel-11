8a792815c46d28835f7b81dbd0ce1be1df3de28b
soru <ityouna007@gmail.com>
2025-01-18 23:47:34 +0900

    refactor: Update list and Use @foreach

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,8 +10,8 @@
     <h1>Posts Index Page</h1>
     <div>My name is {{$username}} and my age is {{$age}} I am gay.</div>
     <ul>
-        <li> {{$posts[0]}} </li>
-        <li> {{$posts[1]}} </li>
-        <li> {{$posts[2]}} </li>
+        @foreach ($posts as $post)
+            <li>{{ $post }}</li>
+        @endforeach
     </ul>
 </html>
