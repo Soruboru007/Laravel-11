f9ceff00c1257c892c01071cddcc50f8509ab3c2
soru <ityouna007@gmail.com>
2025-01-18 22:09:11 +0900

    cleaned code

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::view('/', 'welcome');
 Route::resource('posts', PostController::class);
