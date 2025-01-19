aa70a989c147f582c80dcdf44a5257a61cd16c2b
soru <ityouna007@gmail.com>
2025-01-19 20:10:01 +0900

    feat: Add layout use x-layout component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello World</h1>
-    <form action="/" method="POST">
-        @csrf
-        <input type="text" name="username" id="">
-        <button type="submit">送信</button>
-    </form>
-    <form action="/100" method="POST">
-        @csrf
-        @method('PUT')
-         <input type="text" name="username" id="">
-        <button type="submit">送信</button>
-    </form>
-    <form action="/100" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit">送信削除</button>
-    </form>
-
-</body>
-</html>
+<x-layout>
+    <h1>Welcome Page</h1>
+</x-layout>
