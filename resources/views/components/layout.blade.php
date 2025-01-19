2f1b1b652f81947a942117fec9e846b00276913f
soru <ityouna007@gmail.com>
2025-01-19 20:29:56 +0900

    feat: Add navigation menu and comments

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,11 @@
     <title>Laravel 11 Tutorial</title>
 </head>
 <body>
+    {{-- <nav> は、HTMLの要素の一つで、ナビゲーションエリア（メニュー）を定義するためのセマンティックタグです。主に、ウェブページ内やサイト全体でのリンクをまとめるために使用されます。 --}}
+    <nav>
+        <a href="/">Home</a>
+        <a href="/posts">Posts</a>
+    </nav>
     {{ $slot }}
 </body>
 </html>
