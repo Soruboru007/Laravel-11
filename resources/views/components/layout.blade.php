8f2af79543a4da32ab75a9b8982ed30601bcfe4c
soru <ityouna007@gmail.com>
2025-01-19 21:07:27 +0900

    feat: Enhance navigation menu using Tailwind CSS

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,10 +9,33 @@
 </head>
 <body>
         {{-- <nav> は、HTMLの要素の一つで、ナビゲーションエリア（メニュー）を定義するためのセマンティックタグです。主に、ウェブページ内やサイト全体でのリンクをまとめるために使用されます。 --}}
-    <nav>
-        <a href="/">Home</a>
-        <a href="/posts">Posts</a>
-    </nav>
+            <nav class="bg-gray-900 text-white border-gray-200 fixed top-0 left-0 w-full z-50">
+                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a href="/" class="flex items-center space-x-3">
+                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap">Laravel 11</span>
+                    </a>
+                    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                        </svg>
+                    </button>
+                    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0">
+                            <li>
+                                <a href="/" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-500 md:p-0" aria-current="page">Home</a>
+                            </li>
+                            <li>
+                                <a href="/posts" class="block py-2 px-3 text-gray-300 rounded hover:bg-gray-800 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0">Posts</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+
+
     {{ $slot }}
 </body>
 </html>
