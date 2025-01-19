7311403bee166bc7026b48744b27bd7028c8a719
soru <ityouna007@gmail.com>
2025-01-19 20:46:39 +0900

    chore: Update tailwind to 3.4.17 with Add tailwind

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11 Tutorial</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
         {{-- <nav> は、HTMLの要素の一つで、ナビゲーションエリア（メニュー）を定義するためのセマンティックタグです。主に、ウェブページ内やサイト全体でのリンクをまとめるために使用されます。 --}}
