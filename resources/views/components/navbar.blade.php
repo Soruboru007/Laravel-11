b51ec16b7c7740b15e2fb2104b6b02458e0ee08d
soru <ityouna007@gmail.com>
2025-01-19 22:33:17 +0900

    feat: Add dynamic class binding for navigation links

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,11 +13,18 @@
                   </button>
                   <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        @php
+                            $isActive = false;
+                        @endphp
                       <li>
-                        <a href="/" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+                        <a href="/" @class([
+                            'block py-2 px-3 rounded md:p-0 dark:text-white md:dark:text-blue-500',
+                            'text-white bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500' => $isActive,
+                            'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' => ! $isActive,
+                        ])class="" aria-current="page">Home</a>
                       </li>
                       <li>
-                        <a href="/posts" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Posts</a>
+                        <a href="/posts" class="block py-2 px-3  rounded  md:border-0 md:p-0 ">Posts</a>
                       </li>
                     </ul>
                   </div>
