937a852ebdf020980648cdca9e5946b824af27ca
soru <ityouna007@gmail.com>
2025-01-19 19:07:01 +0900

    feat: Add two view value PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -46,7 +46,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        
+        return view('posts.show', ['post'=> $post]);
     }
 
     /**
@@ -54,7 +54,7 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('posts.edit', ['post'=> $post]);
     }
 
     /**
