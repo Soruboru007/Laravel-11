25df5299f2248d149f047baa7179b1a13f7541bc
soru <ityouna007@gmail.com>
2025-01-20 21:11:37 +0900

    feat: Refactor navbar structure and add navbar-link component

---
+++ resources/views/components/navbar-link.blade.php
@@ -0,0 +1,7 @@
+<li>
+    <a href="/" @class([
+        'block py-2 px-3 rounded md:p-0 dark:text-white md:dark:text-blue-500',
+        'text-white bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500' => $isActive,
+        'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' => ! $isActive,
+    ])class="" aria-current="page">Home</a>
+  </li>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -16,13 +16,7 @@
                         @php
                             $isActive = false;
                         @endphp
-                      <li>
-                        <a href="/" @class([
-                            'block py-2 px-3 rounded md:p-0 dark:text-white md:dark:text-blue-500',
-                            'text-white bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500' => $isActive,
-                            'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' => ! $isActive,
-                        ])class="" aria-current="page">Home</a>
-                      </li>
+
                       <li>
                         <a href="/posts" class="block py-2 px-3  rounded  md:border-0 md:p-0 ">Posts</a>
                       </li>
