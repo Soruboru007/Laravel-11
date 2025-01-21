ff1bed1b522fa671af7af6ea984f64e7d97a4c89
soru <ityouna007@gmail.com>
2025-01-21 23:22:00 +0900

    chore: Log error message "Undefined variable $isActive"

--- resources/views/components/navbar-link.blade.php
+++ resources/views/components/navbar-link.blade.php
@@ -1,11 +1,10 @@
-@php
-    $isActive = false;
-@endphp
+@props(['active' => false])
 
 <li>
-    <a @class([
-        'block py-2 px-3 rounded md:p-0 dark:text-white md:dark:text-blue-500',
+    <a {{ $attributes }} @class([
+        'block py-2 px-3 rounded md:p-0 ',
         'text-white bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500' => $isActive,
-        'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' => ! $isActive,
+        'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white
+        md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' => ! $isActive,
     ])class="" aria-current="page"> {{$slot}} </a>
   </li>
