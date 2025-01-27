57896083e7574b1a4fa7a89deea96e35e42ecf83
soru <ityouna007@gmail.com>
2025-01-27 23:07:58 +0900

    fix: Add missing name attributes to title and content inputs in post creation form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,7 +6,7 @@
             <div class="mb-6">
                 <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900
                 dark:text-white">Tilte</label>
-                <input type="text" id="default-input"
+                <input type="text" id="default-input" name="title"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
                     focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
                     dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
@@ -16,7 +16,7 @@
 
                 <label for="message" class="block mb-2 text-sm font-medium text-gray-900
                 dark:text-white">Content</label>
-                <textarea id="message" rows="4"
+                <textarea id="message" rows="4" name="content"
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border
                     border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700
                     dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
