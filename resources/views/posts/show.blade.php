a76a7c7989d500ecc77b84bea2fe95d66a8047d4
soru <ityouna007@gmail.com>
2025-01-31 12:01:48 +0900

    feat: Enhance post show view layout with improved styling and structure

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,9 @@
 <x-layout>
-    <h1> {{$post->title}} </h1>
-    <main class="max-w-6x1 mx-auto mt-6 lg:mt-20 space-y-6">
-        <p class="test-gray-700"> {{$post->content}} </p>
-    </main>
+    <div class="max-w-2xl mx-auto sm:px-6 lg:px-8 mt-8 bg-slate-50 rounded-lg">
+        <h1 class="text3x1 text-indigo-800 font-semibold"> {{$post->title}} </h1>
+        <main class="max-w-6x1 mx-auto mt-6 lg:mt-20 space-y-6">
+            <p class="test-gray-700 p-4 mb-4"> {{$post->content}} </p>
+        </main>
+
+    </div>
 </x-layout>
