e944b59d9b94db243f4d9de237c841de8377b1fb
soru <ityouna007@gmail.com>
2025-01-31 11:42:35 +0900

    feat: Add content display to post show view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
     <h1> {{$post->title}} </h1>
+    <main class="max-w-6x1 mx-auto mt-6 lg:mt-20 space-y-6">
+        <p class="test-gray-700"> {{$post->content}} </p>
+    </main>
 </x-layout>
